74b8130e87f016f6ef67a1fe1ff8e5d06ea641d9
haimo-26 <79958807+haimo-26@users.noreply.github.com>
2024-05-27 23:37:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -309,6 +309,6 @@ def generate():
 if __name__ == "__main__":
     # engine = initialize_all()
     # app.run(host="0.0.0.0", port=8080)
-    app.run(debug=True)
-    # server = pywsgi.WSGIServer(("0.0.0.0", 8080), app)
+    # app.run(debug=True)
+    server = pywsgi.WSGIServer(("0.0.0.0", 8080), app)
     server.serve_forever()
